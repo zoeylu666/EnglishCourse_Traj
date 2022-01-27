24691bf38f6fb0b91aa294b9579fcf91d1201f82
Zoey Lu <71402093+zoeylu666@users.noreply.github.com>
2022-01-27 02:50:06 -0500

    Add files via upload

--- duPont_Tree.R
+++ duPont_Tree.R
@@ -1,19 +1,22 @@
 # Load library
 # install.packages("collapsibleTree")
 library(collapsibleTree) 
+library(tidyverse)
+library(ggplot2)
 
-# input data must be a nested data frame:
-head(warpbreaks)
 
-# Represent this tree:
-p <- collapsibleTree( warpbreaks, c("wool", "tension", "breaks"))
-p
 
-# save the widget
-# library(htmlwidgets)
-# saveWidget(p, file=paste0( getwd(), "/HtmlWidget/dendrogram_interactive.html"))
+view(duPont)
+head(duPont)
 
 duPont <- read.csv("//uno.oet.udel.edu/CRESP/Projects/CurrentProjects/ZL/ZL Data/ZL_C19_Courses/Course Descriptions/BI Dash/New folder/trial2.csv")
 
-p <- collapsibleTree(df=duPont, c("L1_G9", "L2_G10", "L3_G11", "G4_G12"))
+p <- collapsibleTree(df=duPont, c("L1_G9", "L2_G10", "L3_G11", "L4_G12"), 
+                     tooltip = TRUE)
 p
+
+
+
+# save the widget
+library(htmlwidgets)
+saveWidget(p, file=paste0( getwd(""), "duPont_English_CPHNRS_CT.html"))
