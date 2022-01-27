12246bb3ad8ebb34037ca4e9221fd78593fffcfb
Zoey Lu <71402093+zoeylu666@users.noreply.github.com>
2022-01-27 12:54:54 -0500

    Add files via upload

--- duPont_Tree.R
+++ duPont_Tree.R
@@ -9,7 +9,7 @@ library(ggplot2)
 view(duPont)
 head(duPont)
 
-duPont <- read.csv("//uno.oet.udel.edu/CRESP/Projects/CurrentProjects/ZL/ZL Data/ZL_C19_Courses/Course Descriptions/BI Dash/New folder/trial2.csv")
+duPont <- read.csv("C:/Users/Zoey Lu/Documents/R/Portfolio/CourseTrajectory/AlexiDuPONT.csv")
 
 p <- collapsibleTree(df=duPont, c("L1_G9", "L2_G10", "L3_G11", "L4_G12"), 
                      tooltip = TRUE)
